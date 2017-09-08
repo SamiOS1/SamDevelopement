4cd2269e564541bd1a47d10c7c43c6717f091bc3
SAMEER GHADAGE <sameerghadage@Bnt-MAC-MINI.local>
2017-09-08 11:55:27 +0530

    committed with Amol's B...

--- DemoTest/AppDelegate.swift
+++ DemoTest/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // Girya Bhokat ja
+    // Girya Bhokat ja Amolchya...
     
     var window: UIWindow?
 
